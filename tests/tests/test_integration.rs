81f1ce5774e82d06774afb3efab1579948b75a3a
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-01-27 19:00:12 +0900

    wip

--- tests/tests/test_integration.rs
+++ tests/tests/test_integration.rs
@@ -1,9 +1,31 @@
+use futures::future::Future;
+use maybe_unwind::FutureMaybeUnwindExt as _;
 use mimicaw::{Args, Outcome, Test};
 use polyfuse::Filesystem;
-use std::io;
+use std::{io, panic::UnwindSafe, path::PathBuf};
 use tokio::task;
 
-type TestFn = fn() -> task::JoinHandle<Outcome>;
+fn wrap_test_case<F>(fut: F) -> Box<dyn FnMut() -> task::JoinHandle<Outcome>>
+where
+    F: Future<Output = ()> + UnwindSafe + Send + 'static,
+{
+    let mut fut = Some(fut.maybe_unwind());
+    Box::new(move || {
+        let fut = fut.take().unwrap();
+        task::spawn(async move {
+            match fut.await {
+                Ok(()) => Outcome::passed(),
+                Err(unwind) => {
+                    let msg = match unwind.location() {
+                        Some(loc) => format!("[{}] {}", loc, unwind.payload_str()),
+                        None => unwind.payload_str().to_string(),
+                    };
+                    Outcome::failed().error_message(msg)
+                }
+            }
+        })
+    })
+}
 
 struct TestFs;
 
@@ -11,6 +33,8 @@ impl Filesystem for TestFs {}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    maybe_unwind::set_hook();
+
     let args = Args::from_env().unwrap_or_else(|st| st.exit());
 
     let mountpoint = tempfile::tempdir()?;
@@ -24,25 +48,27 @@ async fn main() -> anyhow::Result<()> {
         }
     });
 
-    let tests: Vec<Test<TestFn>> = vec![
-        Test::test("case1", || task::spawn(async { Outcome::passed() })),
-        Test::test("case2", || {
-            task::spawn(async { Outcome::failed().error_message("foo") })
-        }),
-        Test::<TestFn>::test("case3_a_should_be_zero", || {
-            task::spawn(async move { Outcome::passed() })
-        })
-        .ignore(true),
-    ];
+    let tests = vec![Test::test(
+        "case1",
+        wrap_test_case(case1(mountpoint.path().to_owned())),
+    )];
 
-    let status = mimicaw::run_tests(&args, tests, |_, test_fn: TestFn| {
-        let handle = test_fn();
-        async move { handle.await.unwrap() }
-    })
+    let status = mimicaw::run_tests(
+        &args,
+        tests,
+        |_, mut test_fn: Box<dyn FnMut() -> task::JoinHandle<Outcome>>| {
+            let handle = test_fn();
+            async move { handle.await.unwrap() }
+        },
+    )
     .await;
 
     let _ = tx.send(());
     fs.await??;
 
     status.exit()
+}
+
+async fn case1(_mountpoint: PathBuf) {
+    assert!(false, "explicit assertion fail");
 }
